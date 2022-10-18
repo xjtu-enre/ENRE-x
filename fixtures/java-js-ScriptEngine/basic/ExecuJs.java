5178207f6f07a91eb56768aa68f4670338b92916
DmastLIN <yzl1093822941@163.com>
2022-10-18 21:07:29 +0800

    Update ExecuJs.java

--- fixtures/java-js-ScriptEngine/basic/ExecuJs.java
+++ fixtures/java-js-ScriptEngine/basic/ExecuJs.java
@@ -1,35 +1,13 @@
 import java.io.FileReader;
-import java.util.Scanner;
-import javax.script.Bindings;
 import javax.script.Invocable;
-import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class ExecuJs {
     public static void main(String[] args) throws Exception {
-
-        // get JS ScriptEngine
         ScriptEngine se = new ScriptEngineManager().getEngineByName("javascript");
-        // get variate
-        Bindings bindings = se.createBindings();
-        bindings.put("number", 3);
-        se.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println("input parameter【" + a + "】 + 【" + b + "】");
-            se.eval(new FileReader("D:\\project\\java\\demo\\src\\add.js"));
-            // call or not
-            if (se instanceof Invocable) {
-                Invocable in = (Invocable) se;
-                double result = (double) in.invokeFunction("add", a, b);
-                System.out.println("result：" + result);
-
-            }
-
-        }
-
+        se.eval(new FileReader("D:\\project\\java\\demo\\src\\hello.js"));
+        Invocable in = (Invocable) se;
+        in.invokeFunction("hello");
     }
 }
